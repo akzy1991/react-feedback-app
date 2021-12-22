a15c49be5f72c9dbdf64d2fb1b6da6f536f44490
A <richo_koh@hotmail.com>
2021-12-22 16:46:38 +0800

    added stats

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,17 @@
+const FeedbackStats = ({ feedback }) => {
+  let avgRating =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+
+  avgRating = avgRating.toFixed(1).replace(/[.,]0$/, '');
+
+  return (
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Feedbacks</h4>
+      <h4>Average Rating: {isNaN(avgRating) ? 0 : avgRating}</h4>
+    </div>
+  );
+};
+
+export default FeedbackStats;
