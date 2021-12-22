95d7cfbc38d86374e452b131e04dbcfd18c2c2fa
A <richo_koh@hotmail.com>
2021-12-22 15:12:49 +0800

    added header and feedback item

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  const headerStyles = {
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    color: '#ff6a95',
+  };
+
+  return (
+    <header style={headerStyles}>
+      <div className='container'>
+        <h2>Feedback UI</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
